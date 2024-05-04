e8ab6197a4625ca08b2c66fc7b30f5a2fd75b6b1
Cipher7--Tech <akshayathavale123@gmail.com>
2024-05-04 10:09:59 +0530

    Variables And Data Types

---
+++ Day3.py
@@ -0,0 +1,27 @@
+# In the Day3 I'm Learning About the Introduction to  Variables and DataTypes in Python
+
+# We Simply Declare the variable using = operator
+a = 1
+print(a)
+
+b= 'Akshay'
+print(b)
+
+c = True
+print(c)
+
+# To check The Datatype of the variable we can use type() function
+print("The Type of a", type(a))
+print("The Type of b", type(b))
+print("The Type of c", type(c))
+
+#Sequence Datatypes: list, tuple , Dict
+
+list1 = [8,2.38, "Hello", [5,-4], ["apple","Banana"]]
+print(list1)
+
+tuple1 = ("Apple" , 8, -5,"banana")
+print(tuple1)
+
+dict1= {"Name":"Akshay","Age":21,"canVote":True}
+print(dict1)

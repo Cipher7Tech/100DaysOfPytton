ab414846f1072eddadd1ff48bc48c24f5b05bfca
Cipher7--Tech <akshayathavale123@gmail.com>
2024-05-04 10:03:32 +0530

    First Basic Programs

---
+++ Day1.py
@@ -0,0 +1,16 @@
+""" This Is My 4th Day Of Learnig Python in Privious 3 days I learn About The
+    Basic Structure and History Of Python as Well As THe Modules in Python and
+    PIP in python This Is Officialy First Day Of Learning Python
+    And My Target is Completing 10 days Of learning in One Day
+
+    Date -3/05/2024
+    Time- 01:26PM
+"""
+print("Hello World" , 7)
+print(5)
+print("bye")
+
+
+# The Quiz Print The Poem
+
+print(" Now that the winter's gone, the earth hath lost Her snow-white robes,/n and now no more the frost Candies the grass, or casts an icy cream /n Upon the silver lake or crystal stream; ")

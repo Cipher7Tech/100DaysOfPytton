de872ab0853382cd690cd04e184ffcaae685d87d
Cipher7--Tech <akshayathavale123@gmail.com>
2024-05-04 10:07:18 +0530

    Escape Sequence & Print Statement , Comments

---
+++ Day2.py
@@ -0,0 +1,10 @@
+""" In this  Day 2 Learn About the Comments, Escape Sequences And Print Statement"""
+
+#This A my First Single Line Comment
+print("Since bare repositories are usually shared on some\nsort  of network server, it is usually difficult to cd into the repo and pull changes")
+
+""" This Is My Firt Multiline Comment"""
+
+print("Hey" , 7 ,6, sep="~" , end="009\n")
+print("Hello")
+

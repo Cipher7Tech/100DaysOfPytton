488603a1dee1002aeaea8e5b0028e6508fbd9fe2
Cipher7--Tech <akshayathavale123@gmail.com>
2024-05-04 10:26:18 +0530

    Exercise1 - The Calculator

---
+++ Exercise1.py
@@ -0,0 +1,38 @@
+'''Hello This is the Day 2 of Learning python And Today I will Learn About the
+ python Operators Like Arithmatic And Solving a Exercise of Calculator
+
+ Date- 04-05-2024
+ Day - Saturday
+ Time - 10.15AM
+'''
+
+print("-------The Calculator-------")
+
+a=10
+
+b= 20
+
+print("A value Of a Is :",a)
+print("A value of b Is:",b)
+
+print("-----Addition Operation-----")
+print("a + b = ", a+b)
+
+print("-----Substraction Operation-----")
+print("a - b = ", a-b)
+
+print("-----Multiplication Operation-----")
+print("a * b = ", a*b)
+
+print("-----Division Operation-----")
+print("a / b = ", a/b)
+
+print("-----Modulo Operation-----")
+print("a % b = ", a%b)
+
+print("-----Power Operation-----")
+print("a ** b = ", a**b)
+
+
+
+

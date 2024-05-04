8c065cb94b29d980bca3dd490d798e43d8449e7f
Cipher7--Tech <akshayathavale123@gmail.com>
2024-05-04 10:52:01 +0530

    Typecasting In Python

---
+++ Day4.py
@@ -0,0 +1,41 @@
+"""
+Its A Day2 Of Learning And  I learn About the Typecasting In The Python
+
+ Date- 04-05-2024
+ Day - Saturday
+ Time - 10.31AM
+"""
+
+a = '1'
+
+b='2'
+
+print(a+b)
+print(int(a)+int(b))
+
+
+#There are Two Types of Type Casting Explicit and Implicit
+
+
+#Explicit conversion Done By the Programmer Manually"""
+
+string  = '7'
+num = 6
+
+string_num = int(string)
+sum = num + string_num
+
+print("The Some Of Number and String is :",sum)
+
+
+"""
+Implicit Conversion is done by the Python It Self
+
+Example:
+"""
+c = 2.5
+
+d = 8
+
+print(c + d )
+
